acde620850570e55d2962433ce1f8d831fbba23f
nnadya <n.d.niki2012@gmail.com>
2025-06-30 17:18:35 +0300

    update ManagerRow.tsx

--- src/components/ManagerRow.tsx
+++ src/components/ManagerRow.tsx
@@ -3,20 +3,22 @@ import type { AdminData } from "../types/api";
 
 interface ManagerRowProps {
   admin: AdminData;
+  monthIndices: number[];
 }
 
-const ManagerRow = ({ admin }: ManagerRowProps) => {
+const ManagerRow = ({ admin, monthIndices }: ManagerRowProps) => {
   return (
     <>
       <tr>
         <td className="p-2 border" rowSpan={2}>{admin.adminName}</td>
         <td className="p-2 border">Income</td>
-        {admin.months.map((month, index) => {
+        {monthIndices.map((i) => {
+          const month = admin.months[i];
           const planIncome = month?.plan?.income ?? "No data";
           const factIncome = month?.fact?.income ?? "No data";
       
           return (
-            <React.Fragment key={`row-${admin.id}-${index}`}>
+            <React.Fragment key={`row-${admin.id}-${i}`}>
               <td className="p-2 border text-right">{`$ ${planIncome}`}</td>
               <td className="p-2 border text-right">{`$ ${factIncome}`}</td>
             </React.Fragment>
@@ -25,12 +27,13 @@ const ManagerRow = ({ admin }: ManagerRowProps) => {
       </tr>
       <tr>
         <td className="p-2 border">Active partners</td>
-        {admin.months.map((month, index) => {
+        {monthIndices.map((i) => {
+          const month = admin.months[i];
           const planPartners = month?.plan?.activePartners ?? "No data";
           const factPartners = month?.fact?.activePartners ?? "No data";
       
           return (
-            <React.Fragment key={`partners-${admin.id}-${index}`}>
+            <React.Fragment key={`partners-${admin.id}-${i}`}>
               <td className="p-2 border text-right">{planPartners}</td>
               <td className="p-2 border text-right">{factPartners}</td>
             </React.Fragment>
