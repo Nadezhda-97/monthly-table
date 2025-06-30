e2bb1780a3b0fa31224515ed4127f18718731336
nnadya <n.d.niki2012@gmail.com>
2025-06-30 17:20:56 +0300

    create YearSelect.tsx

---
+++ src/components/tableControls/YearSelect.tsx
@@ -0,0 +1,9 @@
+const YearSelect = () => {
+  return (
+    <select className="border rounded px-3 py-1 text-sm">
+      <option value="2025">Year 2025</option>
+    </select>
+  );
+};
+
+export default YearSelect;
