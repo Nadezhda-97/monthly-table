0cb12e8bb29a546e22d3907a22ac9fd3daf9e65b
nnadya <n.d.niki2012@gmail.com>
2025-06-30 17:17:16 +0300

    update TotalRow.tsx

--- src/components/TotalRow.tsx
+++ src/components/TotalRow.tsx
@@ -1,31 +1,46 @@
 import React from "react";
 import type { TotalItem } from "../types/api";
 
-interface TotalItemProps {
+interface TotalRowProps {
   total: TotalItem[];
+  monthIndices: number[];
 }
 
-const TotalRow = ({ total }: TotalItemProps) => {
+const TotalRow = ({ total, monthIndices }: TotalRowProps) => {
   return (
     <>
       <tr className="bg-gray-50">
         <td className="p-2 border" rowSpan={2}>Manager</td>
         <td className="p-2 border">Total income:</td>
-        {total.map((item, index) => (
-          <React.Fragment key={`total-income-${index}`}>
-            <td className="p-2 border text-right">{`$ ${item.plan.income}`}</td>
-            <td className="p-2 border text-right">{`$ ${item.fact.income}`}</td>
-          </React.Fragment>
-        ))}
+        {monthIndices.map((i) => {
+          const item = total[i];
+          return (
+            <React.Fragment key={`total-income-${i}`}>
+              <td className="p-2 border text-right">
+                {item?.plan ? `$ ${item.plan.income}` : "No data"}
+              </td>
+              <td className="p-2 border text-right">
+                {item?.fact ? `$ ${item.fact.income}` : "No data"}
+              </td>
+            </React.Fragment>
+          );
+        })}
       </tr>
       <tr className="bg-gray-50">
         <td className="p-2 border">Total active partners:</td>
-        {total.map((item, index) => (
-          <React.Fragment key={`total-partners-${index}`}>
-            <td className="p-2 border text-right">{item.plan.activePartners}</td>
-            <td className="p-2 border text-right">{item.fact.activePartners}</td>
-          </React.Fragment>
-        ))}
+        {monthIndices.map((i) => {
+          const item = total[i];
+          return (
+            <React.Fragment key={`total-partners-${i}`}>
+              <td className="p-2 border text-right">
+                {item?.plan ? item.plan.activePartners : "No data"}
+              </td>
+              <td className="p-2 border text-right">
+                {item?.fact ? item.fact.activePartners : "No data"}
+              </td>
+            </React.Fragment>
+          );
+        })}
       </tr>
     </>
   );
