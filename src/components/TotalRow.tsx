5ac0b68299f6d610ee4eb3c761a4542d529fbc9c
nnadya <n.d.niki2012@gmail.com>
2025-07-01 22:04:27 +0300

    update styles

--- src/components/TotalRow.tsx
+++ src/components/TotalRow.tsx
@@ -9,33 +9,33 @@ interface TotalRowProps {
 const TotalRow = ({ total, monthIndices }: TotalRowProps) => {
   return (
     <>
-      <tr className="bg-gray-50">
-        <td className="p-2 border" rowSpan={2}>Manager</td>
-        <td className="p-2 border">Total income:</td>
+      <tr className="bg-gray-50 font-medium text-sm text-blue-900">
+        <td className="p-3 border" rowSpan={2}>Manager</td>
+        <td className="p-3 border">Total income:</td>
         {monthIndices.map((i) => {
           const item = total[i];
           return (
             <React.Fragment key={`total-income-${i}`}>
-              <td className="p-2 border text-right">
+              <td className="p-2 border border-b-0 border-r-0 text-left">
                 {item?.plan ? `$ ${item.plan.income}` : "No data"}
               </td>
-              <td className="p-2 border text-right">
+              <td className="p-2 border border-b-0 border-l-0 text-left">
                 {item?.fact ? `$ ${item.fact.income}` : "No data"}
               </td>
             </React.Fragment>
           );
         })}
       </tr>
-      <tr className="bg-gray-50">
-        <td className="p-2 border">Total active partners:</td>
+      <tr className="bg-gray-50 font-medium text-sm text-blue-900">
+        <td className="p-3 border">Total active partners:</td>
         {monthIndices.map((i) => {
           const item = total[i];
           return (
             <React.Fragment key={`total-partners-${i}`}>
-              <td className="p-2 border text-right">
+              <td className="p-2 border border-t-0 border-r-0 text-left">
                 {item?.plan ? item.plan.activePartners : "No data"}
               </td>
-              <td className="p-2 border text-right">
+              <td className="p-2 border border-t-0 border-l-0 text-left">
                 {item?.fact ? item.fact.activePartners : "No data"}
               </td>
             </React.Fragment>
