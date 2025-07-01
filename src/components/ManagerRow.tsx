3ddb925d30f196fcaa4f062effc7c0ed4e5d1162
nnadya <n.d.niki2012@gmail.com>
2025-07-01 22:06:34 +0300

    add "No data" cell and update styles

--- src/components/ManagerRow.tsx
+++ src/components/ManagerRow.tsx
@@ -10,32 +10,45 @@ const ManagerRow = ({ admin, monthIndices }: ManagerRowProps) => {
   return (
     <>
       <tr>
-        <td className="p-2 border" rowSpan={2}>{admin.adminName}</td>
-        <td className="p-2 border">Income</td>
+        <td className="p-3 border font-bold text-center text-[15px] text-blue-900" rowSpan={2}>{admin.adminName}</td>
+        <td className="p-3 border">Income:</td>
         {monthIndices.map((i) => {
           const month = admin.months[i];
-          const planIncome = month?.plan?.income ?? "No data";
-          const factIncome = month?.fact?.income ?? "No data";
+
+          if (!month) {
+            return (
+              <td
+                key={`node-income-${admin.id}-${i}`}
+                className="p-3 border text-left text-gray-500 italic bg-gray-50"
+                rowSpan={2}
+                colSpan={2}
+              >
+                No data
+              </td>
+            );
+          }
       
           return (
             <React.Fragment key={`row-${admin.id}-${i}`}>
-              <td className="p-2 border text-right">{`$ ${planIncome}`}</td>
-              <td className="p-2 border text-right">{`$ ${factIncome}`}</td>
+              <td className="p-3 border border-b-0 border-r-0 text-left bg-white">{`$ ${month.plan.income}`}</td>
+              <td className="p-3 border border-b-0 border-l-0 text-left bg-white">{`$ ${month.fact.income}`}</td>
             </React.Fragment>
           );
         })}
       </tr>
       <tr>
-        <td className="p-2 border">Active partners</td>
+        <td className="p-3 border">Active partners:</td>
         {monthIndices.map((i) => {
           const month = admin.months[i];
-          const planPartners = month?.plan?.activePartners ?? "No data";
-          const factPartners = month?.fact?.activePartners ?? "No data";
       
+          if (!month) {
+            return null;
+          }
+
           return (
             <React.Fragment key={`partners-${admin.id}-${i}`}>
-              <td className="p-2 border text-right">{planPartners}</td>
-              <td className="p-2 border text-right">{factPartners}</td>
+              <td className="p-3 border border-t-0 border-r-0 text-left bg-white">{month.plan.activePartners}</td>
+              <td className="p-3 border border-t-0 border-l-0 text-left bg-white">{month.fact.activePartners}</td>
             </React.Fragment>
           );
         })}
