51c0aacc95572d2927fab0e687e55d6933e0a7e4
nnadya <n.d.niki2012@gmail.com>
2025-07-01 22:01:58 +0300

    delete h1 and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import Table from "./components/Table";
 
 function App() {
   return (
-    <div className="w-[1440px] h-[1080px] mx-auto p-8">
-      <h1 className="text-2xl font-bold mb-4">Таблица по месяцам</h1>
+    <div className="w-[1440px] h-[1080px] mx-auto p-8 bg-white text-blue-900">
       <Table />
     </div>
   );
