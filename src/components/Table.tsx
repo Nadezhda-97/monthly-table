94cbdf5b5b81be41f82cce750c29e4f23678a35d
nnadya <n.d.niki2012@gmail.com>
2025-07-01 22:57:47 +0300

    update styles

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -53,7 +53,7 @@ const Table = () => {
     <div className="p-4">
       <TableControls onNext={handleNext} onPrev={handlePrev} />
       <div className="overflow-x-auto border rounded-md">
-        <table className="min-w-[900px] w-full border-collapse text-sm text-left text-blue-900 shadow-sm rounded-md overflow-hidden">
+        <table className="min-w-[900px] w-full border-collapse text-sm text-left text-blue-300 shadow-sm rounded-md overflow-hidden">
           <Header months={visibleMonths} />
           <tbody>
             <TotalRow total={total} monthIndices={visibleMonthIndices} />
