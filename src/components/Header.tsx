67e9b7c132c9ade34fb71405695670821076d8a7
nnadya <n.d.niki2012@gmail.com>
2025-07-01 23:01:18 +0300

    update styles, add <th> and paragraphs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header = ({ months }: HeaderProps) => {
   return (
-    <thead className="bg-blue-50 text-[13px] tracking-wide text-blue-900">
+    <thead className="bg-blue-50 text-[13px] tracking-wide text-blue-300">
       <tr>
         <th className="p-3 border font-medium text-center" rowSpan={2}></th>
         <th className="p-3 border font-medium text-center" rowSpan={2}></th>
@@ -15,12 +15,17 @@ const Header = ({ months }: HeaderProps) => {
             {month}
           </th>
         ))}
+        <th className="p-3 border font-medium text-center" rowSpan={2}></th>
       </tr>
       <tr>
         {months.map((_, index) => (
           <React.Fragment key={index}>
-            <th className="p-3 border border-t-0 border-r-0 font-medium text-left text-gray-600">Plan:</th>
-            <th className="p-3 border border-t-0 border-l-0 font-medium text-left text-gray-600">Fact:</th>
+            <th className="p-3 border border-t-0 border-r-0">
+              <p className="font-medium text-left text-gray-300">Plan:</p>
+            </th>
+            <th className="p-3 border border-t-0 border-l-0">
+              <p className="font-medium text-left text-gray-300">Fact:</p>
+            </th>
           </React.Fragment>
         ))}
       </tr>
