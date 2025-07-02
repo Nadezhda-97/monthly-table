8cac82aac68933db9f6dc8aa3d8e8d3ec18c19b3
nnadya <n.d.niki2012@gmail.com>
2025-07-02 21:06:55 +0300

    update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,29 +2,51 @@ import React from "react";
 
 interface HeaderProps {
   months: string[];
+  hoveredColIndex: number | null;
+  setHoveredColIndex: (index: number | null) => void;
 }
 
-const Header = ({ months }: HeaderProps) => {
+const Header = ({ months, hoveredColIndex, setHoveredColIndex }: HeaderProps) => {
   return (
     <thead className="bg-blue-50 text-[13px] tracking-wide text-blue-300">
       <tr>
-        <th className="p-3 border font-medium text-center" rowSpan={2}></th>
-        <th className="p-3 border font-medium text-center" rowSpan={2}></th>
-        {months.map((month) => (
-          <th className="p-3 border border-b-0 font-semibold text-left text-[16px]" colSpan={2} key={month}>
-            {month}
+        <th className="p-3 border border-blue-200 font-medium text-center" rowSpan={2}></th>
+        <th className="p-3 border border-blue-200 font-medium text-center" rowSpan={2}></th>
+        {months.map((month, index) => (
+          <th
+            className="p-3 border border-blue-200 border-b-0 font-semibold text-left text-[16px]"
+            onMouseEnter={() => setHoveredColIndex(index)}
+            onMouseLeave={() => setHoveredColIndex(null)}
+            colSpan={2}
+            key={month}
+          >
+            <p className={`transition-colors duration-150 ${hoveredColIndex === index ? "text-blue-500" : ""}`}>
+              {month}
+            </p>
           </th>
         ))}
-        <th className="p-3 border font-medium text-center" rowSpan={2}></th>
+        <th className="p-3 border border-blue-200 font-medium text-center" rowSpan={2}></th>
       </tr>
       <tr>
         {months.map((_, index) => (
           <React.Fragment key={index}>
-            <th className="p-3 border border-t-0 border-r-0">
-              <p className="font-medium text-left text-gray-300">Plan:</p>
+            <th
+              className="p-3 border border-blue-200 border-t-0 border-r-0"
+              onMouseEnter={() => setHoveredColIndex(index)}
+              onMouseLeave={() => setHoveredColIndex(null)}
+            >
+              <p className={`transition-colors duration-150 font-medium text-left ${hoveredColIndex === index ? "text-gray-500" : "text-gray-300"}`}>
+                Plan:
+              </p>
             </th>
-            <th className="p-3 border border-t-0 border-l-0">
-              <p className="font-medium text-left text-gray-300">Fact:</p>
+            <th
+              className="p-3 border border-blue-200 border-t-0 border-l-0"
+              onMouseEnter={() => setHoveredColIndex(index)}
+              onMouseLeave={() => setHoveredColIndex(null)}
+            >
+              <p className={` transition-colors duration-150 font-medium text-left ${hoveredColIndex === index ? "text-gray-500" : "text-gray-300"}`}>
+                Fact:
+              </p>
             </th>
           </React.Fragment>
         ))}
